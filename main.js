2ac7987b379335589c321ac25d0332f9abed541b
Tar-Nway-Oo <tarnwayo@gmail.com>
2023-10-25 16:53:41 +0630

    add some new features

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-$(".dark-mode").onClick(() => {
-   $(".container").css({backgroundColor: "brown", color: "white"});
-})
+$(".main").hide();
+
+$(".dark-mode").on("click", () => {
+  $(".container").toggleClass("dark");
+});
+
+$(".start").on("click", () => {
+
+  if ($(".input").val().toLowerCase() === "zac") {
+    $(".main").fadeIn(1000, () => {
+      $(".start").fadeOut();
+    });
+
+    $(".warning").fadeOut();
+  } else {
+    $("<h3>Wrong User</h3>").addClass("warning").appendTo("body");
+  }
+
+});
